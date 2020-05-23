e4a798e627dd0c317c79dc31a2ca8d2d4c694e0b
Lauren Frankovich <frankovister@gmail.com>
2020-05-23 17:40:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays!")
+  add_element_to_end_of_array.push("arrays!")
 end
 add_element_to_end_of_array(array, element)
 
