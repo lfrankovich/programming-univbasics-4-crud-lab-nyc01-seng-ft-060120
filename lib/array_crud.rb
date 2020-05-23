74e7a1d384bb9f99c934c5de2098ce315b038311
Lauren Frankovich <frankovister@gmail.com>
2020-05-23 17:41:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["wow", "I", "am", "really", "learning"]
   array.push("arrays!")
 end
 add_element_to_end_of_array(array, element)
